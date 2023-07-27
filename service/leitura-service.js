58ce47ebe36458798285e721f1d1f268e49a7c2a
PipocaPorto <jpportocampos@gmail.com>
2023-07-27 15:30:39 -0300

    Adicionando validação do array

--- service/leitura-service.js
+++ service/leitura-service.js
@@ -1,10 +1,15 @@
 import * as fs from 'node:fs';
+import UserException from '../utils/user-exception.js';
 
 export default class LeituraService {
     lerJSON(jsonPath) {
         let rawData = fs.readFileSync(jsonPath);
 
         let dataJSON = JSON.parse(rawData);
+
+        if (!Array.isArray(dataJSON)) {
+            throw new UserException("O arquivo não contém um array");
+        }
 
         return dataJSON;
     }
---
+++ utils/user-exception.js
@@ -0,0 +1,4 @@
+export default function UserException(message) {
+    this.message = message;
+    this.name = "UserException";
+}
