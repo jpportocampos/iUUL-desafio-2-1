282b9f75eaf8fb302482d31061d8c948540e1554
PipocaPorto <jpportocampos@gmail.com>
2023-07-27 15:25:26 -0300

    Adicionando mensagens de erro

--- validacao.js
+++ validacao.js
@@ -1,26 +1,31 @@
 import MainController from './controller/main-controller.js';
+import SystemException from './utils/system-exception.js';
 
 (function () {
     const controller = new MainController();
 
-    let jsonPath = process.argv.pop();
+    try {
+        let jsonPath = process.argv.pop(); 
+        
+        let dataJSON = controller.leJSON(jsonPath);
 
-    let dataJSON = controller.leJSON(jsonPath);
-
-    let result = '[';
-
-    for (let index = 0; index < dataJSON.length; index++) {
-        let erros = controller.validaJSON(dataJSON[index]);
-        result = result + controller.formataJSON(dataJSON[index], erros);
+        let result = '[';
+    
+        for (let index = 0; index < dataJSON.length; index++) {
+            let erros = controller.validaJSON(dataJSON[index]);
+            result = result + controller.formataJSON(dataJSON[index], erros);
+        }
+    
+        if (result.endsWith("_")) {
+            result = result.replace(/_([^_]*)$/, ']' + '$1');
+        } else if (result === "[") {
+            result = "[]";
+        }
+    
+        result = result.replaceAll('}_', '},');
+    
+        controller.escreveJSON(jsonPath, result);
+    } catch (error) {
+        throw new SystemException("Ocorreu um erro ao processar o arquivo, verifique o caminho passado e tente novamente");
     }
-
-    if (result.endsWith("_")) {
-        result = result.replace(/_([^_]*)$/, ']' + '$1');
-    } else if (result === "[") {
-        result = "[]";
-    }
-
-    result = result.replaceAll('}_', '},');
-
-    controller.escreveJSON(result);
 })();
